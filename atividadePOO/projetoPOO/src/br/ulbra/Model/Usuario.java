0d9491b683160a97552ce31f8815028ecdefd9d0
Arthur schmeling <arthurschmeling08@gmail.com>
2026-04-16 17:14:46 -0300

    Add files via upload

---
+++ atividadePOO/projetoPOO/src/br/ulbra/Model/Usuario.java
@@ -0,0 +1,66 @@
+package br.ulbra.Model;
+
+public class Usuario {
+private int id;
+private String nome; 
+private String email;
+private String senha;
+private String sexo;
+
+    public Usuario() {
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getSenha() {
+        return senha;
+    }
+
+    public void setSenha(String senha) {
+        this.senha = senha;
+    }
+
+    public String getSexo() {
+        return sexo;
+    }
+
+    public void setSexo(String sexo) {
+        this.sexo = sexo;
+    }
+
+    public Usuario(int id, String nome, String email, String senha, String sexo) {
+        this.id = id;
+        this.nome = nome;
+        this.email = email;
+        this.senha = senha;
+        this.sexo = sexo;
+    }
+
+    @Override
+    public String toString() {
+        return "Usuario{" + "id=" + id + ", nome=" + nome + ", email=" + email + ", senha=" + senha + ", sexo=" + sexo + '}';
+    }
+    
+}
